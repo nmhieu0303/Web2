1eb7612a9785c3e3089d92a535b9ef76ce73e03f
Nguyen Minh Hieu <nmhieu03032000@gmail.com>
2021-03-18 09:24:23 +0700

    add feature sum

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-    res.send('Hello World')
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    const { a, b } = req.query;
+    const c = Number(a) + Number(b);
+    res.render('index', { a, b, c });
+});
+app.get('/hello', function(req, res) {
+    const name = req.query.name;
+    res.send(`Hello query ${name}`);
 });
 
-app.listen(PORT, () => {
-    console.log(`listening on port http://localhost:${PORT}`);
+app.get('/hello/:name', function(req, res) {
+    const name = req.params.name;
+    res.send(`Hello param ${name}`);
 });
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));;
