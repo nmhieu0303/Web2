abc25fdb29e764a60a0b5464da7e592958e11d07
Nguyen Minh Hieu <nmhieu03032000@gmail.com>
2021-04-13 20:35:31 +0700

    fix db.js

--- app/models/db.js
+++ app/models/db.js
@@ -1,2 +1,7 @@
 const { Sequelize } = require('sequelize');
-module.exports = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:1@localhost:5432/todo');
+module.exports = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:1@localhost:5432/todo', {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+});
